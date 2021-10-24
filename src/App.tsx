a43a57d6686da3a394c4cc7c09f86eb766b5525a
Denison <denisondealmeidaprtela@hotmail.com>
2021-10-23 21:38:20 -0300

    Subindo aplicação

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useContext, useState } from 'react'
+import logo from './logo.svg'
+import styles from "./App.module.scss"
+import { MessageList } from './components/MessageList'
+import { LoginBox } from './components/LoginBox'
+import { AuthContext } from './contexts/auth'
+import { SendMessageForm } from './components/SendMessageForm'
+
+export function App() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
+      <MessageList />
+      { !!user ? <SendMessageForm /> : <LoginBox /> }
+    </main>
+  )
+}
+
